241528463e1ba2bf8342758346f7c96e7128f8b3
JoaoWebMeneses <joaowebmeneses@gmail.com>
2023-09-02 09:05:07 -0300

    bug arrumado

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const UserRoutes = require('./routes/userRoutes')
+const path = require("path")
 require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 app.use("/auth",UserRoutes)
+app.use("/src/uploads",express.static(path.join(__dirname, 'uploads')))
 
 const PORT = process.env.PORT
 app.listen(PORT,()=>{
